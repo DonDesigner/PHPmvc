d0f435e2cc70cd5fe24fee61853282c565f1f24d
dondesigner <dondesigner@hotmail.com>
2015-11-02 16:20:57 -0200

    Essas classes foram renomeadas para manter o padrão das aulas

---
+++ core/TrabAreas.php
@@ -0,0 +1,11 @@
+<?php
+require_once 'actions/aTrabAreas.php';
+
+/**
+ * Description of TrabAreas
+ *
+ * @author Diego
+ */
+class TrabAreas extends aTrabAreas {
+    //put your code here
+}
---
+++ core/TrabPublicacoes.php
@@ -0,0 +1,10 @@
+<?php
+require_once 'actions/aTrabPublicacao.php';
+/**
+ * Description of TrabPublicacoes
+ *
+ * @author Diego
+ */
+class TrabPublicacoes extends aTrabPublicacao {
+    //put your code here
+}
